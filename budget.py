475e12777678f107cac7ee93798a996305f04c03
rbill314 <rbill314@gmail.com>
2021-09-18 13:35:51 -0400

    added commit

--- budget.py
+++ budget.py
@@ -144,3 +144,18 @@ class Category:
             if item["amount"] < 0:
                 total += item["amount"]
         return total
+
+
+# Thanks to Landon Schlangen who walked me through this FreeCodeCamp Project.
+# The video I watched that (which he was kind enough to take the time to provide)
+# is at his youTube channel Landon Schlangen.. https://www.youtube.com/channel/UC4oRFTHw71_CBSHAcCRmV6w
+# The link is https://www.youtube.com/watch?v=2OL3rbEYWLU
+
+# This is my second time with python and I will come back to this project as I
+# further skills through the other projects.
+# Thank you again to everyone that shares their knowledge to help others learn.
+# Watching a wide variety of people to achieve the same goal in all
+# these videos are doing more to advance my learning and understanding.
+#
+# 9/18/2021
+# Done on VS Code
